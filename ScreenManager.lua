d75a6f865ce6e7ff6dcad712c226cd380cd06b39
Robert Machmer <robert.machmer@gmail.com>
2016-01-12 13:47:07 +0100

    Update license headers

--- ScreenManager.lua
+++ ScreenManager.lua
@@ -1,6 +1,6 @@
 --===============================================================================--
 --                                                                               --
--- Copyright (c) 2014 - 2015 Robert Machmer                                      --
+-- Copyright (c) 2014 - 2016 Robert Machmer                                      --
 --                                                                               --
 -- This software is provided 'as-is', without any express or implied             --
 -- warranty. In no event will the authors be held liable for any damages         --
